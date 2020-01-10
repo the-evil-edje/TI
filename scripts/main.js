33799be907fe7ab1d2901dfbcd23e38f6b7e86ed
Unknown <eddie.beelen@gmail.com>
2020-01-10 16:05:16 +0100

    follows en info added

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  //getCryptoNews();
 
   var getUserInfo1 = $("#getUserInfo1");
 
@@ -10,8 +9,6 @@ $(document).ready(function(){
 
     getUserId(user1);
   });
-
-    // getTest();
 });
 
 function getUserId(user)
@@ -24,7 +21,6 @@ function getUserId(user)
        'Authorization': 'k6gko4szxrctuyuk9i8hzbxw73s9t6',
        'Client-ID': '4apetz69vm7oun8rtshmpi5j4p61ci'
      },
-
 
 	   success: function(info){
 			basicUserInfo1 = info;
@@ -39,16 +35,99 @@ function getUserId(user)
 
         $("#headerTab-1").text(value.login);
 
-      //   html = "<img src=\"" + value.profile_image_url + "\" />";
-      //
-      // $("#fiets").attr("src","https://player.twitch.tv/?channel=" + user);
-      //
-      //
-      // $("#test1").empty().html(user);
-      //
-      // $(".followerInfo").html(html);
+       $("#channel1").attr("src","https://player.twitch.tv/?channel=" + value.login);
+       $("#chat1").attr("src","https://www.twitch.tv/embed/" + value.login + "/chat");
+
+      getUserInformation(value.id);
+      getLiveUserInformation(value.id)
+      getUserFollows(value.id)
 			 })
 	   }
 	});
-  //getCryptoNews();
+}
+
+function getUserInformation(userId) {
+  console.log("jah: " + userId);
+	$.ajax({
+	   type: "GET",
+	   dataType: "json",
+	   url: "https://api.twitch.tv/helix/users?id=" + userId,
+     headers: {
+       'Authorization': 'k6gko4szxrctuyuk9i8hzbxw73s9t6',
+       'Client-ID': '4apetz69vm7oun8rtshmpi5j4p61ci'
+     },
+
+	   success: function(info){
+
+			advancedUserInfo = info;
+
+      $.each(advancedUserInfo.data, function(index, value){
+
+        console.log("jaja: " + value.login);
+        $("#displayName1").html("<h1>Display Name:</h1>" + value.login);
+      })
+    }
+	})
+}
+
+function getLiveUserInformation(userId) {
+	$.ajax({
+	   type: "GET",
+	   dataType: "json",
+	   url: "https://api.twitch.tv/helix/streams?user_id=" + userId,
+     headers: {
+       'Authorization': 'k6gko4szxrctuyuk9i8hzbxw73s9t6',
+       'Client-ID': '4apetz69vm7oun8rtshmpi5j4p61ci'
+     },
+
+	   success: function(info){
+
+			liveUserInfo = info;
+
+      console.log(liveUserInfo);
+
+      $.each(liveUserInfo.data, function(index, value){
+        console.log(value.viewer_count);
+        $("#Viewers1").html("<h1>Viewers:</h1>" + value.viewer_count);
+      })
+    }
+	})
+}
+
+function getUserFollows(userId) {
+	$.ajax({
+	   type: "GET",
+	   dataType: "json",
+	   url: "https://api.twitch.tv/helix/users/follows?to_id=" + userId,
+     headers: {
+       'Authorization': 'k6gko4szxrctuyuk9i8hzbxw73s9t6',
+       'Client-ID': '4apetz69vm7oun8rtshmpi5j4p61ci'
+     },
+
+	   success: function(info){
+
+			userFollows = info;
+
+      $("#Followers").html("<h1>Followers:</h1>" + userFollows.total);
+    }
+	})
+}
+
+function getUserSubscriptions(userId) {
+	$.ajax({
+	   type: "GET",
+	   dataType: "json",
+	   url: "https://api.twitch.tv/helix/users/subscriptions?user_id" + userId,
+     headers: {
+       'Authorization': 'k6gko4szxrctuyuk9i8hzbxw73s9t6',
+       'Client-ID': '4apetz69vm7oun8rtshmpi5j4p61ci'
+     },
+
+	   success: function(info){
+
+			userFollows = info;
+
+      $("#Followers").html("<h1>Followers:</h1>" + userFollows.total);
+    }
+	})
 }
